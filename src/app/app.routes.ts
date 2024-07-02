613bd800e428b681ddb2857baa9534e0b4dd6ff7
gustavoliu <g.liu@nextage.com.br>
2024-07-02 00:30:54 -0300

    Adicionando a homepage.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {Routes} from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Se nenhum caminho for especificado, redireciona para a página inicial (home)
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  // Define a rota para a página inicial (home) e especifica o componente a ser carregado
+  {path: 'home', component: HomeComponent, pathMatch: 'full'},
+];
